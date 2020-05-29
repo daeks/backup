d5f577c9c022325c5c9ec2f8398860c8fd0e3996
daeks <daeks@users.noreply.github.com>
2020-05-29 14:20:12 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ cd $BACKUP_TARGET_DIR
 
 for backupflag in $(find $BACKUP_SOURCE_DIR -name "$BACKUP_FLAG"); do
   if [ -f $backupflag ]; then
-    rsync -rtvph --exclude ".git" $(dirname "$backupflag") $BACKUP_TARGET_DIR -delete
+    rsync -rtvph --exclude '.git' --exclude '*.log' $(dirname "$backupflag") $BACKUP_TARGET_DIR -delete
     
     git add .
     git commit -m "$(date +%s)"
